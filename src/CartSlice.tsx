2f0b2f4139d2a50bfb5df41026ac04816e9efb49
Elle Barnaby <ebarnaby@srcuk.com>
2024-11-15 10:37:55 +0000

    Copying from original

---
+++ src/CartSlice.tsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: [], // Initialize items as an empty arrays
+    },
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload; //gets info out of payload
+            const existingItem = state.items.find(item => item.name === name); //finds the matching name
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+            //if it matches, add 1, if not, add new item
+        },
+        removeItem: (state, action) => {
+            const { name } = action.payload; //gets info out of payload
+            state.items = state.items.filter(item => item.name !== name); //filters if the name matches
+        },
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            if (itemToUpdate) {
+                itemToUpdate.quantity = quantity;
+            }
+        },
+    }
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
